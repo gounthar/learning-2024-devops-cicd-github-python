ff0ea723dff8d79b6fd3c6c514ad4b801596270c
Mathys Caron <caronmathys23@gmail.com>
2024-07-04 15:06:32 +0000

    changement des fichiers calc.py et test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -19,37 +19,34 @@ class TestCalc(unittest.TestCase):
         """
         result = calc.add2(1, 2)
         self.assertEqual(result, 3)
+        
+    def test_add_no_args(self):
+        """
+        Teste l'appel de add2 sans aucun argument. 
+        Vérifie que la fonction renvoie 0 dans ce cas.
+        """
+        self.assertEqual(calc.add2(), 0)
 
-    def test_add_floats(self):
+    def test_add_one_arg(self):
         """
-        Test that the addition of two floats returns the correct result.
+        Teste l'appel de add2 avec un seul argument.
+        Vérifie que la fonction renvoie cet argument.
         """
-        result = calc.add2('10.5', 2)
-        self.assertEqual(result, 12.5)
+        self.assertEqual(calc.add2(5), 5)
 
-    def test_add_strings(self):
+    def test_add_multiple_args(self):
         """
-        Test the addition of two strings returns the two strings as one
-        concatenated string.
+        Teste l'appel de add2 avec plusieurs arguments numériques.
+        Vérifie que la fonction renvoie la somme de ces arguments.
         """
-        result = calc.add2('abc', 'def')
-        self.assertEqual(result, 'abcdef')
+        self.assertEqual(calc.add2(2, 3, 4), 9)
 
-    def test_add_string_and_integer(self):
+    def test_add_string(self):
         """
-        Test the addition of a string and an integer returns them as one
-        concatenated string (in which the integer is converted to a string).
+        Teste l'appel de add2 avec des arguments mixtes, incluant des chaînes de caractères.
+        Vérifie que la fonction additionne les nombres et ignore les chaînes de caractères.
         """
-        result = calc.add2('abc', 3)
-        self.assertEqual(result, 'abc3')
-
-    def test_add_string_and_number(self):
-        """
-        Test the addition of a string and a float returns them as one
-        concatenated string (in which the float is converted to a string).
-        """
-        result = calc.add2('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
+        self.assertEqual(calc.add2(2, 3, "bonjour"), 5)
 
 if __name__ == '__main__':
     '''
