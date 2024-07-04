b33a5d4be2866c91e8ae4540e0616de70a50a763
Thomask25 <140195247+Thomask25@users.noreply.github.com>
2024-07-04 16:25:59 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -6,6 +6,7 @@ NOTE: If a value submitted to the 'add2' function is a float, it must be done so
 in quotes (i.e. as a string).
 '''
 
+
 def conv(value):
     '''
     If 'value' is not an integer, convert it to a float and failing that, a string.
@@ -24,7 +25,8 @@ def conv(value):
         except ValueError:
             return str(value)
 
-def add2(*args):
+
+def add2(arg1, arg2):
     '''
     The 'add2' function itself. It takes two arguments, converts them to their appropriate types
     using the 'conv' function, and adds them together. If either argument is a string, it ensures
@@ -37,16 +39,42 @@ def add2(*args):
     Returns:
     int, float, str: The result of the addition or concatenation.
     '''
- converted_args = [conv(arg) for arg in args]
-    
-    # Vérifier si l'un des arguments est une chaîne
-    if any(isinstance(arg, str) for arg in converted_args):
-        # Convertir tous les arguments en chaînes
-        converted_args = [str(arg) for arg in converted_args]
-    
-    # Additionner ou concaténer tous les arguments
-    result = converted_args[0]
-    for arg in converted_args[1:]:
+    # Convert 'arg1' and 'arg2' to their appropriate types
+    arg1conv = conv(arg1)
+    arg2conv = conv(arg2)
+    # If either 'arg1' or 'arg2' is a string, ensure they're both strings.
+    if isinstance(arg1conv, str) or isinstance(arg2conv, str):
+        arg1conv = str(arg1conv)
+        arg2conv = str(arg2conv)
+    return arg1conv + arg2conv
+
+
+def addAll(args):
+    '''
+    The 'addAll' function itself. It takes multiples arguments, converts them to their appropriate types
+    using the 'conv' function, and adds them together. If either argument is a string, it ensures
+    all are strings before concatenating them.
+
+    Parameter:
+    args (list): A list of values to be added.
+
+    Returns:
+    int, float, str: The result of the addition or concatenation.
+    '''
+    # Create an empty list to store the converted arguments
+    argsconv = []
+
+    # Convert each argument in 'args' to its appropriate type
+    for arg in args:
+        argsconv.append(conv(arg))
+
+    # Create an empty string to store the result
+    result = ""
+    # If any of the arguments are strings, ensure all are strings
+    for arg in argsconv:
+        if (isinstance(arg, str) == False):
+            arg = str(arg)
         result += arg
-    
+
+    # Return the result
     return result
