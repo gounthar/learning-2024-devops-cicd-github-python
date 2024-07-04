fdf4884f1d306b3473b51365eec2ff9e8c6769a1
GeoGeo0059 <geoffrey59171@gmail.com>
2024-07-04 17:23:02 +0200

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,11 +1,53 @@
 import unittest
 import calc
 
-
 class TestCalc(unittest.TestCase):
     """
     TestCalc class inherits from unittest.TestCase for creating unit tests for the calc library.
 
+    Methods:
+    test_add_integers: Test that the addition of two integers returns the correct total.
+    test_add_floats: Test that the addition of two floats returns the correct result.
+    test_add_strings: Test the addition of two strings returns the two strings as one concatenated string.
+    test_add_string_and_integer: Test the addition of a string and an integer returns them as one concatenated string.
+    test_add_string_and_number: Test the addition of a string and a float returns them as one concatenated string.
+    """
+
+    def test_add_integers(self):
+        """
+        Test that the addition of two integers returns the correct total.
+        """
+        result = calc.add2(1, 2)
+        self.assertEqual(result, 3)
+
+    def test_add_floats(self):
+        """
+        Test that the addition of two floats returns the correct result.
+        """
+        result = calc.add2('10.5', 2)
+        self.assertEqual(result, 12.5)
+
+    def test_add_strings(self):
+        """
+        Test the addition of two strings returns the two strings as one
+        concatenated string.
+        """
+        result = calc.add2('abc', 'def')
+        self.assertEqual(result, 'abcdef')
+
+    def test_add_string_and_integer(self):
+        """
+        Test the addition of a string and an integer returns them as one
+        concatenated string (in which the integer is converted to a string).
+        """
+        result = calc.add2('abc', 3)
+        self.assertEqual(result, 'abc3')
+
+    def test_add_string_and_number(self):
+        """
+        Test the addition of a string and a float returns them as one
+        concatenated string (in which the float is converted to a string).
+        """
         result = calc.add2('abc', '5.5')
         self.assertEqual(result, 'abc5.5')
 
@@ -27,11 +69,6 @@ class TestCalc(unittest.TestCase):
 
 if __name__ == '__main__':
     '''
-    The entry point for running the tests from the command line. The unittest.main() function
-    uses a command-line interface and runs all the test methods whose names start with 'test'.
-    '''
-    unittest.main()
-    unittest.main()
     The entry point for running the tests from the command line. The unittest.main() function
     uses a command-line interface and runs all the test methods whose names start with 'test'.
     '''
