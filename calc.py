48671be50f58c05ea7b80b8e2ed3aad6bc9463a7
yann.bauduin <bauduin.yann.59@gmail.com>
2024-07-04 16:25:04 +0200

    ont y retourne

--- calc.py
+++ calc.py
@@ -5,14 +5,24 @@ def conv(val):
         return val
 
 def add2(*args):
-    somme = 0
+    if all(isinstance(arg, (int, float)) for arg in args):
+        somme = 0
+    else:
+        somme = ''
+    
     for arg in args:
         if isinstance(arg, (int, float)):
-            somme += arg
+            if isinstance(somme, (int, float)):
+                somme += arg
+            else:
+                somme += str(arg)
         elif isinstance(arg, str):
             converted = conv(arg)
             if isinstance(converted, (int, float)):
-                somme += converted
+                if isinstance(somme, (int, float)):
+                    somme += converted
+                else:
+                    somme += str(converted)
             else:
-                somme = str(somme) + converted
+                somme += converted
     return somme
