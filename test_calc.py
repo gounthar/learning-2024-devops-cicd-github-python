d8f7c86fc553cecd010f432af0b9dcc9e69b6c17
FlorianMargage <florian.margage@hotmail.com>
2024-07-04 16:17:41 +0200

    upload finished TP

--- test_calc.py
+++ test_calc.py
@@ -1,6 +1,7 @@
 import unittest
 import calc
 
+
 class TestCalc(unittest.TestCase):
     """
     TestCalc class inherits from unittest.TestCase for creating unit tests for the calc library.
@@ -17,14 +18,14 @@ class TestCalc(unittest.TestCase):
         """
         Test that the addition of two integers returns the correct total.
         """
-        result = calc.add2(1, 2)
+        result = calc.addAll([1, 2])
         self.assertEqual(result, 3)
 
     def test_add_floats(self):
         """
         Test that the addition of two floats returns the correct result.
         """
-        result = calc.add2('10.5', 2)
+        result = calc.addAll(['10.5', 2])
         self.assertEqual(result, 12.5)
 
     def test_add_strings(self):
@@ -32,7 +33,7 @@ class TestCalc(unittest.TestCase):
         Test the addition of two strings returns the two strings as one
         concatenated string.
         """
-        result = calc.add2('abc', 'def')
+        result = calc.addAll(['abc', 'def'])
         self.assertEqual(result, 'abcdef')
 
     def test_add_string_and_integer(self):
@@ -40,16 +41,24 @@ class TestCalc(unittest.TestCase):
         Test the addition of a string and an integer returns them as one
         concatenated string (in which the integer is converted to a string).
         """
-        result = calc.add2('abc', 3)
+        result = calc.addAll(['abc', 3])
         self.assertEqual(result, 'abc3')
 
-    def test_add_string_and_number(self):
+    def test_add_strings_and_numbers(self):
         """
-        Test the addition of a string and a float returns them as one
+        Test the addition of multiples strings and floats returns them as one
         concatenated string (in which the float is converted to a string).
         """
-        result = calc.add2('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
+        result = calc.addAll(['abc', '5.5', 'def', '49'])
+        self.assertEqual(result, 'abc5.5def49')
+
+    def test_avoid(self):
+        """
+        Test if no arguments are passed to the addAll function.
+        """
+        result = calc.addAll([])
+        self.assertEqual(result, 0)
+
 
 if __name__ == '__main__':
     '''
