36aaa5f5e25ec27b15ddda3bf6dd3336ad299578
arthur59930 <arthur.hallez.59930@gmail.com>
2024-07-04 14:16:05 +0000

    Fin de l'exercice*

--- calc.py
+++ calc.py
@@ -6,6 +6,7 @@ NOTE: If a value submitted to the 'add2' function is a float, it must be done so
 in quotes (i.e. as a string).
 '''
 
+
 def conv(value):
     '''
     If 'value' is not an integer, convert it to a float and failing that, a string.
@@ -23,6 +24,7 @@ def conv(value):
             return float(value)
         except ValueError:
             return str(value)
+
 
 def add2(arg1, arg2):
     '''
@@ -45,3 +47,34 @@ def add2(arg1, arg2):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
     return arg1conv + arg2conv
+
+
+def addAll(args):
+    '''
+    The 'addAll' function itself. It takes multiples arguments, converts them to their appropriate types
+    using the 'conv' function, and adds them together. If either argument is a string, it ensures
+    all are strings before concatenating them.
+
+    Parameter:
+    args (list): A list of values to be added.
+
+    Returns:
+    int, float, str: The result of the addition or concatenation.
+    '''
+    # Create an empty list to store the converted arguments
+    argsconv = []
+
+    # Convert each argument in 'args' to its appropriate type
+    for arg in args:
+        argsconv.append(conv(arg))
+
+    # Create an empty string to store the result
+    result = ""
+    # If any of the arguments are strings, ensure all are strings
+    for arg in argsconv:
+        if (isinstance(arg, str) == False):
+            arg = str(arg)
+        result += arg
+
+    # Return the result
+    return result
