c73ccf0d5d4f0929909ed0fd8070788be1dfd90f
Tristan Riboulet <116078722+TristanRib@users.noreply.github.com>
2024-07-04 16:25:26 +0200

    doc(alc.py): Update doc

--- calc.py
+++ calc.py
@@ -7,6 +7,15 @@ in quotes (i.e. as a string).
 '''
 
 def conv(value):
+    '''
+    If 'value' is not an integer, convert it to a float and failing that, a string.
+
+    Parameters:
+    value (int, float, str): The value to be converted.
+
+    Returns:
+    int, float, str: The converted value.
+    '''
     try:
         return int(value)
     except ValueError:
@@ -16,6 +25,17 @@ def conv(value):
             return str(value)
 
 def add2(*args):
+    '''
+    The 'add2' function itself. It takes an undefined number of arguments, converts them to their appropriate types
+    using the 'conv' function, and adds them together. If either argument is a string, it ensures
+    all are strings before concatenating them.
+
+    Parameters:
+    args (list): The list of args passed to the function.
+
+    Returns:
+    int, float, str: The result of the addition or concatenation.
+    '''
     args = [conv(arg) for arg in args]
 
     if any(isinstance(arg, str) for arg in args):
