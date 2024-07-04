55e364928ef2c091ee1c9f98fef76a57a4167362
Lucas Barbier <barbier.lucas08@gmail.com>
2024-07-04 14:11:19 +0000

    doc : task 3

--- calc.py
+++ calc.py
@@ -1,9 +1,7 @@
 '''
-The 'calc' library contains the 'add2' function that takes 2 values and adds
-them together. If either value is a string (or both of them are) 'add2' ensures
-they are both strings, thereby resulting in a concatenated result.
-NOTE: If a value submitted to the 'add2' function is a float, it must be done so
-in quotes (i.e. as a string).
+The 'calc' library contains the 'add2' function that takes infinite amount of values and adds
+them together. If one of the values are a string (or both of them are) 'add2' ensures
+they'are being ignored, thereby resulting in a addition of the result of the int and float values.
 '''
 
 def conv(value):
@@ -25,7 +23,13 @@ def conv(value):
             return str(value)
 
 def add2(*args):
+    """
+    Addition an infinite amount of int & float values.
+    Otherwise, the value will be ignored.
 
+    *args: args to addition.
+    Returns: the addition of the args.
+    """
     somme = 0
     for arg in args:
         if isinstance(arg, (int, float)):
--- test_calc.py
+++ test_calc.py
@@ -3,64 +3,19 @@ import calc
 
 class TestCalc(unittest.TestCase):
     """
-    TestCalc class inherits from unittest.TestCase for creating unit tests for the calc library.
-
+    Tests for validating the functionality of the `calc` file.
+    
     Methods:
-    test_add_integers: Test that the addition of two integers returns the correct total.
-    test_add_floats: Test that the addition of two floats returns the correct result.
-    test_add_strings: Test the addition of two strings returns the two strings as one concatenated string.
-    test_add_string_and_integer: Test the addition of a string and an integer returns them as one concatenated string.
-    test_add_string_and_number: Test the addition of a string and a float returns them as one concatenated string.
+    test_add_no_args: Test that calling add without arguments returns 0.
+    test_add_one_arg: Test that calling add with a single argument returns the argument itself.
+    test_add_multiple_args: Test the addition of multiple args returns addition of them all.
+    test_add_with_string: Test the addition of a string and other args returns them as addition of them all without paying attention to string.
     """
-
-    # def test_add_integers(self):
-    #     """
-    #     Test that the addition of two integers returns the correct total.
-    #     """
-    #     result = calc.add2(1, 2)
-    #     self.assertEqual(result, 3)
-
-    # def test_add_floats(self):
-    #     """
-    #     Test that the addition of two floats returns the correct result.
-    #     """
-    #     result = calc.add2('10.5', 2)
-    #     self.assertEqual(result, 12.5)
-
-    # def test_add_strings(self):
-    #     """
-    #     Test the addition of two strings returns the two strings as one
-    #     concatenated string.
-    #     """
-    #     result = calc.add2('abc', 'def')
-    #     self.assertEqual(result, 'abcdef')
-
-    # def test_add_string_and_integer(self):
-    #     """
-    #     Test the addition of a string and an integer returns them as one
-    #     concatenated string (in which the integer is converted to a string).
-    #     """
-    #     result = calc.add2('abc', 3)
-    #     self.assertEqual(result, 'abc3')
-
-    # def test_add_string_and_number(self):
-    #     """
-    #     Test the addition of a string and a float returns them as one
-    #     concatenated string (in which the float is converted to a string).
-    #     """
-    #     result = calc.add2('abc', '5.5')
-    #     self.assertEqual(result, 'abc5.5')
     
     def test_add_no_args(self):
-        """
-        Test that calling add without arguments returns 0.
-        """
         self.assertEqual(calc.add2(), 0)
 
     def test_add_one_arg(self):
-        """
-        Test that calling add with a single argument returns the argument itself.
-        """
         self.assertEqual(calc.add2(5), 5)
 
     def test_add_multiple_args(self):
