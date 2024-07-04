8216e5a66fdf1f30407cb8dae45a52c62f12db1b
Algamor59 <34629228+Algamor59@users.noreply.github.com>
2024-07-04 16:33:17 +0200

    Update test_calc.py to pass

--- test_calc.py
+++ test_calc.py
@@ -31,14 +31,14 @@ class TestCalc(unittest.TestCase):
         """
         Test that the addition of two floats returns the correct result.
         """
-        result = calc.add2('10.5', 2)
+        result = calc.add2(10.5, 2)
         self.assertEqual(result, 12.5)
 
     def test_add__many_floats(self):
         """
         Test that the addition of many floats returns the correct result.
         """
-        result = calc.add2('10.5', 2, '5.5')
+        result = calc.add2(10.5, 2, 5.5)
         self.assertEqual(result, 18)
 
 if __name__ == '__main__':
