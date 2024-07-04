ff76479b74d6f2e85f66164d2545814656b3e4fa
romain belhis <romainbelhis59@hotmail.com>
2024-07-04 14:06:25 +0000

    Ajout list argument.

--- calc.py
+++ calc.py
@@ -1,13 +1,14 @@
-'''
+"""
 The 'calc' library contains the 'add2' function that takes 2 values and adds
 them together. If either value is a string (or both of them are) 'add2' ensures
 they are both strings, thereby resulting in a concatenated result.
 NOTE: If a value submitted to the 'add2' function is a float, it must be done so
 in quotes (i.e. as a string).
-'''
+"""
+
 
 def conv(value):
-    '''
+    """
     If 'value' is not an integer, convert it to a float and failing that, a string.
 
     Parameters:
@@ -15,7 +16,7 @@ def conv(value):
 
     Returns:
     int, float, str: The converted value.
-    '''
+    """
     try:
         return int(value)
     except ValueError:
@@ -24,8 +25,9 @@ def conv(value):
         except ValueError:
             return str(value)
 
-def add2(arg1, arg2):
-    '''
+
+def add(args: list):
+    """
     The 'add2' function itself. It takes two arguments, converts them to their appropriate types
     using the 'conv' function, and adds them together. If either argument is a string, it ensures
     both are strings before concatenating them.
@@ -36,12 +38,11 @@ def add2(arg1, arg2):
 
     Returns:
     int, float, str: The result of the addition or concatenation.
-    '''
-    # Convert 'arg1' and 'arg2' to their appropriate types
-    arg1conv = conv(arg1)
-    arg2conv = conv(arg2)
-    # If either 'arg1' or 'arg2' is a string, ensure they're both strings.
-    if isinstance(arg1conv, str) or isinstance(arg2conv, str):
-        arg1conv = str(arg1conv)
-        arg2conv = str(arg2conv)
-    return arg1conv + arg2conv
+    """
+    somme = 0
+    for arg in args:
+        if isinstance(arg, (int, float)):
+            somme += conv(arg)
+        elif isinstance(arg, str):
+            somme += conv(arg)
+    return somme
--- test_calc.py
+++ test_calc.py
@@ -1,6 +1,7 @@
 import unittest
 import calc
 
+
 class TestCalc(unittest.TestCase):
     """
     TestCalc class inherits from unittest.TestCase for creating unit tests for the calc library.
@@ -13,18 +14,20 @@ class TestCalc(unittest.TestCase):
     test_add_string_and_number: Test the addition of a string and a float returns them as one concatenated string.
     """
 
+    LISTE_VALEUR = (1, 2, 3, 4)
+
     def test_add_integers(self):
         """
         Test that the addition of two integers returns the correct total.
         """
-        result = calc.add2(1, 2)
-        self.assertEqual(result, 3)
+        result = calc.add((1, 2, 3, 4))
+        self.assertEqual(result, 10)
 
     def test_add_floats(self):
         """
         Test that the addition of two floats returns the correct result.
         """
-        result = calc.add2('10.5', 2)
+        result = calc.add((1.5, 4, 3, 4))
         self.assertEqual(result, 12.5)
 
     def test_add_strings(self):
@@ -32,28 +35,29 @@ class TestCalc(unittest.TestCase):
         Test the addition of two strings returns the two strings as one
         concatenated string.
         """
-        result = calc.add2('abc', 'def')
-        self.assertEqual(result, 'abcdef')
+        result = calc.add(("abc", "def"))
+        self.assertEqual(result, "abcdef")
 
     def test_add_string_and_integer(self):
         """
         Test the addition of a string and an integer returns them as one
         concatenated string (in which the integer is converted to a string).
         """
-        result = calc.add2('abc', 3)
-        self.assertEqual(result, 'abc3')
+        result = calc.add(("adc", 3))
+        self.assertEqual(result, "abc3")
 
     def test_add_string_and_number(self):
         """
         Test the addition of a string and a float returns them as one
         concatenated string (in which the float is converted to a string).
         """
-        result = calc.add2('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
+        result = calc.add(("abc", 5.5))
+        self.assertEqual(result, "abc5.5")
 
-if __name__ == '__main__':
-    '''
+
+if __name__ == "__main__":
+    """
     The entry point for running the tests from the command line. The unittest.main() function
     uses a command-line interface and runs all the test methods whose names start with 'test'.
-    '''
+    """
     unittest.main()
